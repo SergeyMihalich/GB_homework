6b16aed56afa609e89f150b1045cd5334be5a077
SergeyMihailovich <fsm_mail.ru>
2022-12-26 11:51:38 +0300

    сделал ДЗ задача 10

---
+++ C_sharp/sem_2/homework/homework_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+
+int TwoNumberGettingNumbers(int number)
+{
+    int digit = number / 10 % 10;
+    return digit;
+}
+
+int RandomNumber = GetNumberFromRange(100, 999);
+int TwoNumber = TwoNumberGettingNumbers(RandomNumber);
+Console.WriteLine($"вторая цифрв числа {RandomNumber} равна {TwoNumber}");

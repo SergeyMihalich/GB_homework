55d6b2ddae5d8daff434be65aa135eb3f41a5956
SergeyMihailovich <fsm_mail.ru>
2022-12-26 13:42:00 +0300

    сделал ДЗ задача 15

---
+++ C_sharp/sem_2/homework/homework_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+void SearchWeekend(int NumDay)
+{
+    if (NumDay >= 1 && NumDay <= 5)
+    {
+        Console.WriteLine("это будничный день");
+    }
+    else if (NumDay == 6 || NumDay == 7)
+    {
+        Console.WriteLine("это выходной день");
+    }
+    else
+    {
+        Console.WriteLine("такого дня в недели нет");
+    }
+}
+
+Console.Write("Введите число: ");
+SearchWeekend(int.Parse(Console.ReadLine()!));

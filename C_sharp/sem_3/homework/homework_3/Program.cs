1165c24f9c304d4e08050caf67febf2b3a15534c
SergeyMihailovich <fsm_mail.ru>
2022-12-29 19:22:57 +0300

    сделал ДЗ семинар 3

---
+++ C_sharp/sem_3/homework/homework_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int getUserValue(string masssege)
+{
+    Console.WriteLine(masssege);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int num = getUserValue("введите число") + 1;
+
+for (int i = 0; i < num; i++)
+{
+    if (i < num - 1)
+    {
+        Console.Write($"{Math.Pow(i,3)}, ");
+    }
+    else
+    {
+        Console.Write($"{Math.Pow(i,3)}");
+    }
+    
+}

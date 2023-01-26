1ff8701625f8fa4d02e00967ba747750be39e70f
SergeyMihailovich <fsm_mail.ru>
2023-01-26 13:47:44 +0300

    семинар 6

---
+++ C_sharp/sem_6/homework_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Необходимое количество чисел: ");
+int CountNum = Convert.ToInt32(Console.ReadLine());
+
+int[] GetNumbers()
+{
+    int[] InputNumbers = new int [CountNum];
+    for (int i = 0; i < CountNum; i++)
+    {
+        Console.Write($"Введите число №{i+1}: ");
+        InputNumbers[i] = Convert.ToInt32(Console.ReadLine()!);
+    }
+    return InputNumbers;
+}
+
+void CountNumbers(int[] InputNumbers)
+{
+    int sum = 0;
+    for (int i = 0; i < InputNumbers.Length; i++)
+    {
+        if (InputNumbers[i] > 0)
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"количество значений больше 0 = {sum}");
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+        if (i != numbers.Length - 1)
+        {
+            Console.Write($"{numbers[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{numbers[i]}]");
+        }
+    Console.WriteLine();
+}
+
+int[] InputNumbers = GetNumbers();
+CountNumbers(InputNumbers);
+PrintArray(InputNumbers);
---
+++ C_sharp/sem_6/homework_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double GetNumbers(string N)
+{
+    Console.WriteLine($"введите значение {N}");
+    double Num = Convert.ToInt32(Console.ReadLine());
+    return Num;
+}
+
+void GetCoordinates(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"две прямые пересекутся в точке: ({x}, {y})");
+}
+
+double b1 = GetNumbers("b1");
+double k1 = GetNumbers("k1");
+double b2 = GetNumbers("b2");
+double k2 = GetNumbers("k2");
+
+GetCoordinates(b1, k1, b2, k2);
+

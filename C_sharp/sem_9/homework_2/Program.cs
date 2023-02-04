3c238df41e5cd8d1382c58786db24acdeeaeea64
SergeyMihailovich <fsm_mail.ru>
2023-02-04 21:20:54 +0300

    семинар 9

---
+++ C_sharp/sem_9/homework_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int numberM = ReadInt("Введите начальное число M:");
+int numberN = ReadInt("Введите начальное число N:");
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+GapNumberSum(numberM, numberN, 0);
---
+++ C_sharp/sem_9/homework_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int m = ReadInt("Введите m: ");
+int n = ReadInt("Введите n: ");
+int functionAkkerman = Ack(m, n);
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+Console.Write($"A(m,n) = {functionAkkerman} ");
